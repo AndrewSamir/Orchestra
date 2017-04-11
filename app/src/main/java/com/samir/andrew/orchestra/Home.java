ebd5d8c76a3b77412321817ddc8c10066a1878b8
andrew <andrew_samir93@hotmail.com>
2017-04-12 01:33:49 +0200

    pass code

--- app/src/main/java/com/samir/andrew/orchestra/Home.java
+++ app/src/main/java/com/samir/andrew/orchestra/Home.java
@@ -1,10 +1,13 @@
 package com.samir.andrew.orchestra;
 
+import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,15 +17,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.samir.andrew.orchestra.Adapters.HomePagerAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private ViewPager mViewPager;
 
     public static boolean firstFragment = false;
-
+    Dialog loadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,9 +116,11 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-      /*  if (id == R.id.nav_camera) {
+        if (id == R.id.nav_pass_code) {
+
+            passcodeDialog();
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } /*else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -111,4 +136,146 @@ public class Home extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void passcodeDialog() {
+
+        // custom dialog
+        final Dialog dialog = new Dialog(Home.this);
+        dialog.setContentView(R.layout.custom_passcode_dialog);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+
+        // set the custom dialog components - text, image and button
+        final EditText text = (EditText) dialog.findViewById(R.id.editTextPassCodeCustomDialog);
+
+
+        Button Cancel = (Button) dialog.findViewById(R.id.buttonCancelCustomDialog);
+        // if button is clicked, close the custom dialog
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        Button Link = (Button) dialog.findViewById(R.id.buttonLinkAccountCustomDialog);
+        Link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (text.getText().length() == 0) {
+                    text.setError("Please Enter Your PassCode");
+                    return;
+                } else if (!text.getText().toString().contains("@")) {
+                    text.setError("Please Enter a Vaild PassCode");
+                    return;
+                } else {
+                    dialog.dismiss();
+                    loadingdialogFunction();
+                    String[] separated = text.getText().toString().split("@");
+
+
+                    getUriFromFirebase(separated[1], text.getText().toString().replaceAll(" ", "%20"));
+
+                }
+
+            }
+        });
+        dialog.show();
+    }
+
+    private void getUriFromFirebase(final String Project, final String passCode) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("CompaniesURI/" + Project);
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+
+                Log.d("value", "Value is: " + value);
+
+                getUnitData(value + "/GetUnitInfo?PassCode=" + passCode, Project);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("value", "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+
+    private void loadingdialogFunction() {
+
+        loadingDialog = new Dialog(Home.this);
+        loadingDialog.setContentView(R.layout.loading_dialog);
+
+        loadingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        loadingDialog.show();
+
+    }
+
+    private void getUnitData(String url, final String project) {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(Home.this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("responseData", response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONObject object = jsonObject.getJSONObject("UnitData");
+                            String unitCode = object.getString("UnitCode");
+
+                            pushDataToFirebase(unitCode, response, project);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // get the data from firebase
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private void pushDataToFirebase(String unitKey, String value, String project) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(value);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users/GPaLxYkiaVPROyvqFQ9hUr8HmTt1/Projects/" + project + "/" + unitKey);
+
+        myRef.setValue(value, new DatabaseReference.CompletionListener() {
+            public void onComplete(DatabaseError error, DatabaseReference ref) {
+                loadingDialog.dismiss();
+                Toast.makeText(Home.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
 }
